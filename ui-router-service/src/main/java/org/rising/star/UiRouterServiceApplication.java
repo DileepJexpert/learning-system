139829912cef3a2a36fb12a204cb83ec99bd418b
DileepkMaurya <todileepmaurya@gmail.com>
2021-09-24 14:30:47 +0530

    add

---
+++ ui-router-service/src/main/java/org/rising/star/UiRouterServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rising.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiRouterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiRouterServiceApplication.class, args);
+	}
+
+}
---
+++ ui-router-service/src/test/java/org/rising/star/UiRouterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rising.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiRouterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
